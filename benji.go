881ade12f43f0bf160b358bbf9ec051fd70425e8
uzudil <hunhm1@gmail.com>
2020-07-13 22:05:57 -0700

    minor reorg

--- benji.go
+++ benji.go
@@ -2,31 +2,9 @@ package main
 
 import (
 	"flag"
-	"os"
 
-	"github.com/uzudil/benji4000/bscript"
-	"github.com/uzudil/benji4000/gfx"
-	"github.com/uzudil/benji4000/sound"
+	"github.com/uzudil/benji4000/core"
 )
-
-func repl(video *gfx.Gfx, sound *sound.Sound) {
-	bscript.Repl(video, sound)
-}
-
-func run(source string, scale int, fullscreen, nosound, showAst *bool) {
-	video := gfx.NewGfx(scale, *fullscreen)
-	sound := sound.NewSound(*nosound)
-	if source != "" {
-		go func() {
-			bscript.Run(source, showAst, nil, video, sound)
-			os.Exit(0)
-		}()
-	} else {
-		go repl(video, sound)
-	}
-
-	video.Render.MainLoop()
-}
 
 func main() {
 	var source string
@@ -39,5 +17,5 @@ func main() {
 	flag.IntVar(&scale, "scale", 2, "Image scale factor")
 	flag.Parse()
 
-	run(source, scale, fullscreen, nosound, showAst)
+	core.Run(source, scale, fullscreen, nosound, showAst)
 }
